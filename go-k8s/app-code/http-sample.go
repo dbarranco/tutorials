b56ba72ee601b1e3cb2c40f8ccad2a0be236375a
Beltran Rueda <beltran@bitnami.com>
2017-06-01 17:04:03 +0200

    Go hello world app

--- go-k8s/app-code/http-sample.go
+++ go-k8s/app-code/http-sample.go
@@ -11,5 +11,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":3000", nil)
 }
